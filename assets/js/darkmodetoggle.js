46cf39f1f5ea722c5864c996b7d408129f7c5c98
Suk Yee <yongsukyee@gmail.com>
2023-07-26 13:52:55 +1000

    update darkmode toggle

--- assets/js/darkmodetoggle.js
+++ assets/js/darkmodetoggle.js
@@ -1,15 +1,33 @@
 /*
 Toggle Dark-mode Button
-References: https://dev.to/albertomontalesi/add-dark-mode-to-your-website-with-just-a-few-lines-of-code-5baf
-            https://medium.com/geekculture/how-to-toggle-between-light-dark-mode-7efcb48d6211
+References: https://traveling-coderman.net/code/light-mode/
+            https://www.albertovarela.net/blog/2022/07/implementing-light-dark-mode/
+            https://dev.to/albertomontalesi/add-dark-mode-to-your-website-with-just-a-few-lines-of-code-5baf
             https://ryanfeigenbaum.com/dark-mode/
+            https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
 */
 
-document.getElementById('toggleTheme').addEventListener('click', function (e) {
-    document.body.classList.toggle('dark');
-    const target = e.target;
-    target.classList.toggle('fa-moon');
-    target.classList.toggle('fa-sun');
-    
-    localStorage.setItem("colorTheme", document.body.classList.contains("dark") ? "dark" : "light");
+function isLight() {
+    return localStorage.getItem("light-mode");
+}
+
+function toggleRootClass() {
+    document.querySelector("body").classList.toggle("light");
+}
+
+function toggleLocalStorageItem() {
+    if (isLight()) {
+        localStorage.removeItem("light-mode");
+    } else {
+        localStorage.setItem("light-mode", "set");
+    }
+}
+
+if (isLight()) {
+    toggleRootClass();
+}
+
+document.querySelector(".theme-icon").addEventListener("click", function () {
+    toggleLocalStorageItem();
+    toggleRootClass();
 });
